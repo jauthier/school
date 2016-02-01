daed3acf752b3118f63183b4ecf9a94a7311a14b
Jessica Authier <jauthier@longbottom.socs.uoguelph.ca>
2016-02-01 17:50:16 -0500

    go

--- A1.c
+++ A1.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <dirent.h>
 
-void commandMenu(char ** args, int numArgs);
+int commandMenu(char ** args, int numArgs);
 int lsNoArgs();
 int checkPipe(char *str);
 int checkInRedir(char *str);
@@ -15,19 +15,19 @@ int main(int argc, char argv[]){
     char buffer[100];
     char * args[8];
     int done = 0;
-    int i=0;
     char *token;
+    int i=0;
 
     while (done == 0){
 
+		i = 0;
         printf("> ");
         fgets(buffer, 100, stdin);
-
+		printf("%s",buffer);
         token = strtok(buffer, " \n");
         while (token!= NULL){
-            args[i]= malloc(strlen(token)+1);
+            args[i]= malloc((strlen(token)+1)*sizeof(char));
             strcpy(args[i], token);
-            printf("%s\n",args[i]);
             token = strtok(NULL, " \n");
             i++;
         }
@@ -37,53 +37,64 @@ int main(int argc, char argv[]){
         } else if (checkInRedir(buffer)==1){
              
         } else if (checkOutRedir(buffer)==1){
-            
-        } else if (){
-            
+
         } else {
             commandMenu(args, i); //i is one more then there are args
         }
         
         
+        int j=0;
+        for (j=0;j<i;j++){
+			free(args[j]);
+		}
         
         
-        
-        done =1;
     }
     return 0;
 }
 
-void commandMenu(char ** args, int numArgs){
+int commandMenu(char ** args, int numArgs){
     
+
+	if (numArgs == 0){
+		return 0;
+	}
     if (strcmp(args[0],"exit")==0){
-        printf("logout\n\n[Process completed]");
+        printf("logout\n\n[Process completed]\n");
         exit(0);
     } else if (strcmp(args[0],"ls")==0){
-        if (i == 1){//no arguments
-            
-        }
+		if (numArgs == 1){//no arguments
+            lsNoArgs();
+        }else if (strcmp(args[1],"-a")==0){
+            lsAll();
+		}
+
         
     }
     
 }
 
 int lsNoArgs(){
-    
+
     struct dirent * dirEntry;
     char cwd[200];
     getcwd(cwd, sizeof(cwd));
     DIR *dir = opendir(cwd);
-    
+
     if (dir == NULL){
         printf("\n");
         return 0;
     }
-    
+
     while ((dirEntry = readdir(dir)) != NULL) {
-        if (dirENtry->d_name[0] != '.') {
+        if (dirEntry->d_name[0] != '.') {
             printf("%s\n",dirEntry->d_name);
         }
     }
+}
+
+int lsAll(){
+	printf("here");
 }
 
 int checkPipe(char *str){
