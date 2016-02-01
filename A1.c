f9a4fd3578d4760ed8f6ac74c72e26c7cd5a28f5
Jessica Authier <jauthier@longbottom.socs.uoguelph.ca>
2016-02-01 16:27:06 -0500

    go

--- A1.c
+++ A1.c
@@ -1,24 +1,41 @@
-#import <stdio.h>
-#import <stdlib.h>
-#import <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int checkPipe(char *str);
 int checkInRedir(char *str);
 int checkOutRedir(char *str);
 
 int main(int argc, char argv[]){
-    
-    char * string1 = "ls -a | cat"
-    
-    if (checkPipe(string1)==1){
+
+    char buffer[100] = "ls -a | cat";
+    char * args[8];
+	int done = 0;
+	int i=0;
+	char *token;
+
+	while (done == 0){
+
+		token = strtok(buffer, " \n");
+		while (token!= NULL){
+			args[i]= malloc(strlen(token)+1);
+			strcpy(args[i], token);
+			printf("%s\n",args[i]);
+			token = strtok(NULL, " \n");
+			i++;
+		}
+
+
+    	if (checkPipe(buffer)==1){
         
+    	}else if (checkInRedir(buffer)==1){
         
-    }else if (checkInRedir(string1)==1){
+    	}else if (checkOutRedir(buffer)==1){
         
-    }else if (checkOutRedir(string1)==1){
-        
-    }
-    
+    	}
+		done =1;
+	}
+    return 0;
 }
 
 int checkPipe(char *str){
@@ -27,7 +44,7 @@ int checkPipe(char *str){
     int i = 0;
     char letter;
     
-    for (i=0;1<len;1++){
+    for (i=0;i<len;i++){
         letter = str[i];
         if (letter == '|'){
             return 1;
@@ -42,7 +59,7 @@ int checkInRedir(char *str){
     int i = 0;
     char letter;
     
-    for (i=0;1<len;1++){
+    for (i=0;i<len;i++){
         letter = str[i];
         if (letter == '<'){
             return 1;
@@ -57,7 +74,7 @@ int checkOutRedir(char *str){
     int i = 0;
     char letter;
     
-    for (i=0;1<len;1++){
+    for (i=0;i<len;i++){
         letter = str[i];
         if (letter == '>'){
             return 1;
