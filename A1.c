2e0eb6b3d7aa34e67d8932afe4f8e2f005e0801c
jauthier <jauthier@mail.uoguelph.ca>
2016-02-01 18:23:30 -0500

    go

--- A1.c
+++ A1.c
@@ -63,51 +63,11 @@ int commandMenu(char ** args, int numArgs){
         printf("logout\n\n[Process completed]\n");
         exit(0);
     } else if (strcmp(args[0],"ls")==0){
-		if (numArgs == 1){//no arguments
-            lsNoArgs();
-        }else if (strcmp(args[1],"-a")==0){
-            lsAll();
-		}
+        
 
         
     }
     
-}
-
-int lsNoArgs(){
-
-    struct dirent * dirEntry;
-    char cwd[200];
-    getcwd(cwd, sizeof(cwd));
-    DIR *dir = opendir(cwd);
-
-    if (dir == NULL){
-        printf("\n");
-        return 0;
-    }
-
-    while ((dirEntry = readdir(dir)) != NULL) {
-        if (dirEntry->d_name[0] != '.') {
-            printf("%s\n",dirEntry->d_name);
-        }
-    }
-}
-
-int lsAll(){
-    struct dirent * dirEntry;
-    char cwd[200];
-    getcwd(cwd, sizeof(cwd));
-    DIR *dir = opendir(cwd);
-
-    if (dir == NULL){
-        printf("\n");
-        return 0;
-    }
-
-    while ((dirEntry = readdir(dir)) != NULL) {
-        printf("%s\n",dirEntry->d_name);
-    }
-
 }
 
 int checkPipe(char *str){
--- ls.c
+++ ls.c
@@ -1,3 +1,46 @@
 #include <stdio.h>
 
+int lsMenu (){
+    if (numArgs == 1){//no arguments
+        lsNoArgs();
+    }else if (strcmp(args[1],"-a")==0){
+        lsAll();
+    }
+    
+}
 
+int lsNoArgs(){
+
+    struct dirent * dirEntry;
+    char cwd[200];
+    getcwd(cwd, sizeof(cwd));
+    DIR *dir = opendir(cwd);
+
+    if (dir == NULL){
+        printf("\n");
+        return 0;
+    }
+
+    while ((dirEntry = readdir(dir)) != NULL) {
+        if (dirEntry->d_name[0] != '.') {
+            printf("%s\n",dirEntry->d_name);
+        }
+    }
+}
+
+int lsAll(){
+    struct dirent * dirEntry;
+    char cwd[200];
+    getcwd(cwd, sizeof(cwd));
+    DIR *dir = opendir(cwd);
+
+    if (dir == NULL){
+        printf("\n");
+        return 0;
+    }
+
+    while ((dirEntry = readdir(dir)) != NULL) {
+        printf("%s\n",dirEntry->d_name);
+    }
+
+}
