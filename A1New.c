a081f4de705dd1365eaeabdc94c62c36a8516275
jauthier <jauthier@mail.uoguelph.ca>
2016-02-08 16:06:38 -0500

    going

--- A1New.c
+++ A1New.c
@@ -3,9 +3,11 @@
 #include <string.h>
 #include <signal.h>
 
-int checkPipe(char *str);
-int checkInRedir(char *str);
-int checkOutRedir(char *str);
+
+void execProcess(char **args);
+int checkPipe(char **args, int numArgs);
+int checkInRedir(char **args, int numArgs);
+int checkOutRedir(char **args, int numArgs);
 int checkBackground(char **args, int numArgs);
 
 
@@ -41,77 +43,121 @@ int main(int argc, char * argv[]){
 		}
 		args[i] = NULL;
 		
-		pid_t pid = fork();
-		
-		if (pid == 0){
-			printf("In child\n");
-			
-			if (checkBackground(args,i) == 1){
-				args[i-1] = NULL;
-				isBackground = 1;
-			}
-			printf("%d\n",isBackground);
-			
-			
-		} else if (pid > 0){
-			printf("In parent\n");
-		}else {
-			printf("Fork Failed\n");
+		if (checkBackground(args,i) == 1){
+			args[i-1] = NULL;
+			isBackground = 1;
 		}
+			
+		if (isBackground == 1)
+			forkInBack();
+		else
+			forkInFore();
+	}
+}
+
+void forkInBack (){
+	int status;
+	pid_t pid = fork();
 		
+	if (pid == 0){
+		printf("In child\n");
 		
+		if (checkPipe(args, i)==1)
+			
+		else if (checkInRedir(args, i)==1)
+			
+		else if (checkOutRedir(args, i)==1)
+			
+		else
+			execProcess(args);
 		
+	} else if (pid > 0){
+		printf("In parent\n");
+		waitpid(pid, &status, WNOHANG);
 		
+	}else {
+		printf("Fork Failed\n");
+	}
+}
+
+void forkInFore (){
+	int status;
+	pid_t pid = fork();
+		
+	if (pid == 0){
+		printf("In child\n");
+		
+		if (checkPipe(args, i)==1)
+			
+		else if (checkInRedir(args, i)==1)
+			
+		else if (checkOutRedir(args, i)==1)
+			
+		else
+			execProcess(args);
+		
+	} else if (pid > 0){
+		printf("In parent\n");
+		waitpid(pid, &status, 0);
+		
+	}else {
+		printf("Fork Failed\n");
 	}
 	
 }
 
+
+
+	
+void execProcess(char **args){
+			
+		
+	char *path = "/bin/";
+	char fullPath[20];
+
+	strcpy(fullPath, path);
+	srtcat(fullPath, args[0]);
+	execvp(fullPath, args);
+}
+
 /*returns 1 if there is a |*/
-int checkPipe(char *str){
-    
-    int len = strlen(str);
-    int i = 0;
-    char letter;
-    
-    for (i=0;i<len;i++){
-        letter = str[i];
-        if (letter == '|'){
-            return 1;
-        }
-    }
-    return 0;
+int checkPipe(char **args, int numArgs){
+
+	int i = 0;
+
+	for (i=0;i<numArgs;i++){
+		if (strcmp(args[i], "|")==0){
+			return 1;
+		}
+	}
+	return 0;
 }
 
 /*returns 1 if ther is a <*/
-int checkInRedir(char *str){
-    
-    int len = strlen(str);
-    int i = 0;
-    char letter;
-    
-    for (i=0;i<len;i++){
-        letter = str[i];
-        if (letter == '<'){
-            return 1;
-        }
-    }
-    return 0;
+int checkInRedir(char **args, int numArgs){
+
+	int i = 0;
+
+	for (i=0;i<numArgs;i++){
+		if (strcmp(args[i], "<")==0){
+			return 1;
+		}
+	}
+	return 0;
 }
 
 /*returns 1 if ther is a >*/
-int checkOutRedir(char *str){
+int checkOutRedir(char **args, int numArgs){
+
+	int i = 0;
+
+	for (i=0;i<numArgs;i++){
+		if (strcmp(args[i], ">")==0){
+			return 1;
+		}
+	}
+	return 0;
     
-    int len = strlen(str);
-    int i = 0;
-    char letter;
-    
-    for (i=0;i<len;i++){
-        letter = str[i];
-        if (letter == '>'){
-            return 1;
-        }
-    }
-    return 0;
 }
 
 /*returns 1 if ther is a &*/
