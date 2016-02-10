339ce2dfb555dafd2677d534cb5d3b250a30fc6f
jauthier <jauthier@mail.uoguelph.ca>
2016-02-10 16:00:38 -0500

    pleasework

--- forkTest.c
+++ forkTest.c
@@ -20,302 +20,350 @@ int piping(char **args, int numArgs);
 
 int main(int argc, char * argv[]){
 
-	char buffer[500];
-	char *args[20];
-	char *token;
-	int i;
-	int isBackground;
-		struct sigaction sig;
-		sig.sa_sigaction = childHandler;
+    char buffer[500];
+    char *args[20];
+    char *token;
+    int i;
+    int isBackground;
+        struct sigaction sig;
+        sig.sa_sigaction = childHandler;
 
-	do{
-		int status = 0;
-		isBackground = 0;
+    do{
+        int status = 0;
+        isBackground = 0;
 
-		printf("> ");
-		fgets(buffer, 500, stdin);
+        printf("> ");
+        fgets(buffer, 500, stdin);
 
-		//checks if the command is exit
-		if (strcmp(buffer, "exit\n")==0){
-			printf("logout\n\n[Process completed]\n");
-			exit(0);
-		}
-		
-		token = strtok(buffer, " \n");
-		i = 0;
-		// parse the command line input
-		while(token!=NULL){
-			args[i]= malloc((strlen(token)+1)*sizeof(char));
+        //checks if the command is exit
+        if (strcmp(buffer, "exit\n")==0){
+            printf("logout\n\n[Process completed]\n");
+            exit(0);
+        }
+
+        token = strtok(buffer, " \n");
+        i = 0;
+        // parse the command line input
+        while(token!=NULL){
+            args[i]= malloc((strlen(token)+1)*sizeof(char));
             strcpy(args[i], token);
-			token = strtok(NULL, " \n");
-			i++;
-		}
-		args[i] = NULL;
+            token = strtok(NULL, " \n");
+            i++;
+        }
+        args[i] = NULL;
 
-		pid_t pid = fork();
-		int type = 0;
-		
-		if (checkBackground(args,i) == 1){
-			args[i-1] = NULL;
-			isBackground = 1;
-		}
-		
-		if (pid == 0){
-			
-			printf("In Child\n");
-			menu(args, i);
-			exit(0);
-			
-		}else if(pid > 0){
-			
-			printf("In parent");
-			if (isBackground == 1)
-				type = WNOHANG;
-			waitpid(pid,&status,type);
-			sigaction(SIGCHLD,&sig,NULL);
+        pid_t pid = fork(); //create a child process
+        int type = 0;
 
-		}else {
-			printf("Fork Error");
-		}
-		
-		int j=0;
-		for (j=0;j<i;j++){
-			free(args[j]);
-	}
-		
-	}while(1);
-	
-	return 0;
+        if (checkBackground(args,i) == 1){
+            args[i-1] = NULL;
+            isBackground = 1;
+        }
+
+        if (pid == 0){
+            menu(args, i);
+            exit(0);
+        }else if(pid > 0){
+            if (isBackground == 1)
+                type = WNOHANG;
+            waitpid(pid,&status,type);
+            sigaction(SIGCHLD,&sig,NULL);
+        }else {
+            printf("Fork Error");
+        }
+
+        int j=0;
+        for (j=0;j<i;j++){
+            free(args[j]);
+        }
+    }while(1);
+    return 0;
 }
 
+/*
+ * Checks for any operators in the command line input
+ */
 void menu(char** args, int i){
-	printf("In Menu\n");
-	fflush(stdout);
-
-	if (checkPipe(args, i)==1){
-		printf("|\n");
-		piping(args, i);
-
-	}else if (checkInRedir(args, i)==1){
-		printf("<");
-	}else if (checkOutRedir(args, i)==1){
-		outRedir(args,i);
-	}else{
-		execProcess(args, i);
-	}
-	printf("Menu end\n");
+    
+    if (checkPipe(args, i)==1){
+        piping(args, i);
+    }else if (checkInRedir(args, i)==1){
+        printf("<");
+    }else if (checkOutRedir(args, i)==1){
+        outRedir(args,i);
+    }else{
+        execProcess(args, i);
+    }
 }
 
+/*
+ * Executes the users command
+ */
 void execProcess(char **args, int numArgs){
 
-	int test;
-	char tempStr[20];
-	strcpy(tempStr, args[0]);
+    int test;
+    char tempStr[20];
+    strcpy(tempStr, args[0]);
 
-	//if the process is an executable
-	if (tempStr[0] == '.' && tempStr[1] == '/' ){
+    //if the process is an executable
+    if (tempStr[0] == '.' && tempStr[1] == '/' ){
 
-		int len = strlen(tempStr);
-		char execName[len];
-		int i =0;
-		//take the first char* out of the array
-		char * newArgs[15];
-		i=0;
-		for (i=0;i<numArgs-1;i++){
-			newArgs[i] = args[i+1];
-		}
-		newArgs[i] = NULL;
+        int len = strlen(tempStr);
+        char execName[len];
+        int i =0;
+        //take the first char* out of the array
+        char * newArgs[15];
+        i=0;
+        for (i=0;i<numArgs-1;i++){
+            newArgs[i] = args[i+1];
+        }
+        newArgs[i] = NULL;
 
-		execvp(tempStr, newArgs);
+        execvp(tempStr, newArgs);
 
+    }else{
+        char *path = "/bin/";
+        char fullPath[20];
 
-		printf("\n%s\n",execName);
-
-
-	}else{
-		char *path = "/bin/";
-		char fullPath[20];
-
-		strcpy(fullPath, path);
-		strcat(fullPath, args[0]);
-		test = execvp(fullPath, args);
-		if (test == -1)
-			printf("%s: command not found\n",args[0]);
-	}
+        strcpy(fullPath, path);
+        strcat(fullPath, args[0]);
+        test = execvp(fullPath, args);
+        if (test == -1)
+            printf("%s: command not found\n",args[0]);
+    }
 }
 
 void childHandler(int signal, siginfo_t* signalInfo, void *hold){
 
-	int status = 0;
-	printf("process complete");
-	if (signal == SIGCHLD)
-		waitpid(signalInfo->si_pid,&status,0);
-
+    int status = 0;
+    printf("process complete");
+    if (signal == SIGCHLD)
+        waitpid(signalInfo->si_pid,&status,0);
 }
 
 /*returns 1 if there is a |*/
 int checkPipe(char **args, int numArgs){
 
-	int i = 0;
-
-	for (i=0;i<numArgs;i++){
-		if (strcmp(args[i], "|")==0){
-			return 1;
-		}
-	}
-	return 0;
+    int i = 0;
+    for (i=0;i<numArgs;i++){
+        if (strcmp(args[i], "|")==0){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 /*returns 1 if ther is a <*/
 int checkInRedir(char **args, int numArgs){
 
-	int i = 0;
-
-	for (i=0;i<numArgs;i++){
-		if (strcmp(args[i], "<")==0){
-			return 1;
-		}
-	}
-	return 0;
+    int i = 0;
+    for (i=0;i<numArgs;i++){
+        if (strcmp(args[i], "<")==0){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 /*returns 1 if ther is a >*/
 int checkOutRedir(char **args, int numArgs){
 
-	int i = 0;
-	for (i=0;i<numArgs;i++){
-		if (strcmp(args[i], ">")==0){
-			return 1;
-		}
-	}
-	return 0;
-    
+    int i = 0;
+    for (i=0;i<numArgs;i++){
+        if (strcmp(args[i], ">")==0){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 /*returns 1 if ther is a &*/
 int checkBackground(char **args, int numArgs){
 
-	int i = 0;
-
-	for (i=0;i<numArgs;i++){
-		if (strcmp(args[i], "&")==0){
-			return 1;
-		}
-	}
-	return 0;
+    int i = 0;
+    for (i=0;i<numArgs;i++){
+        if (strcmp(args[i], "&")==0){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 
 
 int outRedir(char **args, int numArgs){
 
-	int i = 0;
-	int j = 0;
-	char outFile[20];
-	char *newArgs[10];
-	FILE *fp;
+    int i = 0;
+    int j = 0;
+    char outFile[20];
+    char *newArgs[10];
+    FILE *fp;
 
-	//determines where in the array the > is
-	for (i=0;i<numArgs;i++){
-		if (strcmp(args[i], ">")==0)
-			break;
-	}
-	//gets the name of the file
-	if (args[i+1] != NULL){
-		strcpy(outFile, args[i+1]);
-	} else {
-		printf("No file\n");
-		return 0;
-	}
-	//make a new array with the commands that willbe sent to execProcess
-	for (j=0;j<i;j++){
-		newArgs[j] = args[j];
-	}
-	newArgs[j] = NULL;
+    //determines where in the array the > is
+    for (i=0;i<numArgs;i++){
+        if (strcmp(args[i], ">")==0)
+            break;
+    }
+    //gets the name of the file
+    if (args[i+1] != NULL){
+        strcpy(outFile, args[i+1]);
+    } else {
+        printf("No file\n");
+        return 0;
+    }
+    //make a new array with the commands that willbe sent to execProcess
+    for (j=0;j<i;j++){
+        newArgs[j] = args[j];
+    }
+    newArgs[j] = NULL;
 
-	//open file stream
-	fp = freopen(outFile,"w",stdout);
-	if (fp == NULL){
-		printf("File not found\n");
-		return 0;
-	}
-	//send to get executed
-	execProcess(newArgs, numArgs);
-	//close file
-	fclose(fp);
+    //open file stream
+    fp = freopen(outFile,"w",stdout);
+    if (fp == NULL){
+        printf("File not found\n");
+        return 0;
+    }
+    //send to get executed
+    execProcess(newArgs, j);
+    //close file
+    fclose(fp);
 
-	return 0;
+    return 0;
 }
 
-//int inRedir()
+int inRedir(char ** args, int numArgs){
+    //command < filename
+    //read file and give it as input to the command
+    
+    int i = 0;
+    int j = 0;
+    char inFile[20];
+    char *newArgs[10];
+    FILE *fp;
+    char buffer[200];
+    char *token;
+    
+    /* determines where in the array the < is */
+    for (i=0;i<numArgs;i++){
+        if (strcmp(args[i], "<")==0)
+            break;
+    }// i give the position of the <
+
+    //gets the name of the file
+    if (args[i+1] != NULL){
+        strcpy(inFile, args[i+1]);
+    } else {
+        printf("No file\n");
+        return 0;
+    }
+    
+    //make a new array with the commands that willbe sent to execProcess
+    for (j=0;j<i;j++){
+        newArgs[j] = args[j];
+    }
+    newArgs[j] = NULL;
+    
+    //open file stream
+    fp = fopen(inFile,"r");
+    if (fp == NULL){
+        printf("File not found\n");
+        return 0;
+    }
+    
+    fgets(buffer, 200, fp);
+    
+     //add the read in string to the end of the char* array
+        token = strtok(buffer, " \n");
+        n = j;
+        while(token!=NULL){
+            strcpy(newArgs[n], token);
+            token = strtok(NULL, " \n");
+            n++;
+        }
+        newArgs[n] = NULL;
+    
+    
+    //send to get executed
+    execProcess(newArgs, j);
+    //close file
+    fclose(fp)
+    
+    return 0;
+}
 
 int piping (char **args, int numArgs){
 
-	int i = 0;
+    int i = 0;
+    int j = 0;
+    char * pipeOut[5];
+    char * pipeIn[100];
 
-	char * pipeOut[5];
-	char * pipeIn[100];
+    int fd[2];
+    int pipeError;
+    char buffer[200];
+    char *token;
 
-	int pipe1[2];
-	int pipeError;
-	char buffer[200];
+    /* determines where in the array the | is */
+    for (i=0;i<numArgs;i++){
+        if (strcmp(args[i], "|")==0)
+            break;
+    }// i give the position of the |
 
+    /* the stuff before the | goes into pipeOut */
+    for (j=0;j<i;j++){
+        pipeOut[j] = args[j];
+    }
+    pipeOut[j] = NULL;
 
-	//determines where in the array the | is
-	for (i=0;i<numArgs;i++){
-		if (strcmp(args[i], "|")==0)
-			break;
-	}// i give the position of the |
+    /* the stuff after the | goes into pipeIn */
+    int m = i+1; //start with the element after the |
+    int k = 0;
+    for (m=i+1;m<numArgs;m++ ){
+        pipeIn[k] = args[m];
+        k++;
+    }
+    pipeIn[k] = NULL;
+    
+    /* make sure pipe worked */
+    pipeError = pipe(fd);
+    if (pipeError ==-1){
+        printf("Pipe Error\n");
+        return 0;
+    }
 
-	//the stuff before the | goes into pipeOut
-	int j = 0;
-	for (j=0;j<i;j++){
-		pipeOut[j] = args[j];
-		printf("%d: %s\n",j,pipeOut[j]);
-	}
-	pipeOut[j] = NULL;
+    // make a new process to execute the first command	
+    pid_t childpid = fork(); 
 
-	//the stuff after the | goes into pipeIn
-	int m = i+1; //start with the element after the |
-	int k = 0;
-	for (m=i+1;m<numArgs;m++ ){
-		pipeIn[k] = args[m];
-		printf("%d: %s\n",k,pipeIn[k]);
-		k++;
-	}
-	pipeIn[k] = NULL;
+    if (childpid < 0)
+        printf("Failed\n");
+    else if (childpid == 0){
 
+        close(1);//close stdout
+        close(fd[0]);
+        //switch the output, anything printed to stdout goes to the pipe
+        dup2(fd[1], 1);
+        /*send the pipeOut array, and its size to the execProcess function */ 
+        execProcess(pipeOut,j); 
 
-	pipeError = pipe(pipe1);
-	pid_t childpid = fork();
+        exit(0);
 
-	if (childpid < 0)
-		printf("Failed\n");
-	else if (childpid == 0){
-
-		close(1);
-		close(pipe1[0]);
-		//switch the output, anything printed to stdout goes to the pipe
-		dup2(pipe1[1], 1);
-		execProcess(pipeOut,j);
-
-		exit(0);
-
-	}else {
-		close(0);
-		close(pipe1[1]);
-		read(pipe1[0],buffer,200);
-		printf("%s\n",buffer);
-		//add the read in string to the end of the char* array
-
-
-
-
-
-	}
-
-
-
-
-	return 0;
+    }else {
+        close(0);//close stdin
+        close(fd[1]);
+        read(fd[0],buffer,200);
+        
+        //add the read in string to the end of the char* array
+        token = strtok(buffer, " \n");
+        n = k;
+        while(token!=NULL){
+            strcpy(pipeIn[n], token);
+            token = strtok(NULL, " \n");
+            n++;
+        }
+        pipeIn[n] = NULL;
+        execvp(pipeIn, n);
+    }
+    close(fd[0]);
+    close(fd[1]);
+    return 0;
 }
