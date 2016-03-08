b9187baccca4c8a8b19993bee085ee8cc1b5748a
jauthier <jauthier@mail.uoguelph.ca>
2016-03-08 18:23:46 -0500

    go

--- Threads/pthreads.c
+++ Threads/pthreads.c
@@ -96,7 +96,8 @@ char* getLine(FILE* fp, char *line){
 void createNewProcess(FILE *fp, int id, int threadNum){
     
     char buffer[100];
-    process newProcess = (process*)malloc(sizeof(process));
+    process newProcess;
+    newProcess = malloc(sizeof(process));
     newProcess.pid = id;
     newProcess.numThreads = threadNum;
     
