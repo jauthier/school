756664127a2895c3ecc79e7921e42f66dfeb90a4
jauthier <jauthier@mail.uoguelph.ca>
2016-03-08 18:13:48 -0500

    go

--- Threads/pthreads.c
+++ Threads/pthreads.c
@@ -6,17 +6,18 @@
 
 char *getLine(FILE *fp, char *line);
 
-struct thread {
+typedef struct thread {
     int tid;
     int cpuTime;
     int ioTime;
     struct thread *next;
 } thread;
 
-struct Process {
+typedef struct process {
     int pid;
     int numThreads;
     struct thread firstThread;
+    struct process *next;
 } process;
 
 
@@ -27,6 +28,7 @@ int main (int argc, char *argv){
     int threadSwitchTime;
     int processSwitchTime;
     char buffer [500];
+    process *processList;
     
     //for parsing
     //first line <how many processes, num of units to switch
@@ -49,6 +51,12 @@ int main (int argc, char *argv){
     
     printf("there are %d processes, %d, %d.\n", numProcesses, threadSwitchTime, processSwitchTime);
     
+    //get second line, process number and number of threads
+    char *processInfo = getLine(fp, buffer);
+    token = strtok(processInfo, " ");
+    int processNum = atol(token);
+    token = strtok(NULL, " ");
+    int threadNum = atol(token);
     
     return 0;
 }
@@ -81,5 +89,68 @@ char* getLine(FILE* fp, char *line){
     return line;
 }
 
+void createNewProcess(FILE *fp, int id, int threadNum){
+    
+    char buffer[100];
+    struct process newProcess = malloc(sizeof(struct process));
+    newProcess.pid = id;
+    newProcess.numThreads = threadNum;
+    
+    //need to make the thread list
+    //use the number of threads to get the next n lines and add them to the list
+    struct thread *threadList;
+    struct thread threadToAdd;
+    
+    //get the info if the first thread
+    char *tempLine = getLine(fp, buffer);
+    int i = 0;
+    char *token;
+    int threadID;
+    int cpuTime;
+    int ioTime;
+    
+    for (i=0;i>numThreads;i++){
+        token = strtok(tempLine, " ");
+        threadID = atol(token);
+        token = strtok(NULL, " ");
+        cpuTime = atol(token);
+        token = strtok(NULL, " ");
+        ioTime = atol(token);
+        threadToAdd = createNewThread(threadID, cpuTime, ioTime);
+        threadList = addThread(threadToAdd, threadList);
+        
+    }
+    
+    
+}
 
+thread createNewThread(int threadID, int cpuTime, int ioTime){
+    thread newThread = malloc(sizeof(thread));
+    newThread->tid = threadID;
+    newThread->cpuTime = cpuTime;
+    newThread->ioTime = ioTime;
+    newThread->next = NULL;
+    
+    return newThread;
+}
+
+thread *addThread(thread threadToAdd, thread *threadList){
+    if (threadList == NULL)
+        threadList = threadToAdd;
+    else {
+        //find the last item in the list
+        
+        int check = 0;
+        while (check==0){
+            thread *currentThread  = threadList;
+            if (currentThread->next == NULL){
+                check = 1;
+                currentThread->next = threadToAdd;
+                
+            } else 
+                currentThread = currentThread.next;    
+        }
+    }
+    return threadList;
+}
 
