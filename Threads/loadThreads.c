7b258e2c63a89a1fd84c6dce09c932d3bea4c39d
jauthier <jauthier@mail.uoguelph.ca>
2016-03-09 14:08:14 -0500

    go

--- Threads/loadThreads.c
+++ Threads/loadThreads.c
@@ -251,8 +251,9 @@ int addIOTime(burst *burstList){
         currentBurst = currentBurst->next;
     }
     //get the last one
+    printf("%d\n",currentBurst->ioTime);
     sum = sum + currentBurst->ioTime;
-    
+    printf("%d\n",currentBurst->ioTime);
     return sum;
     
     
